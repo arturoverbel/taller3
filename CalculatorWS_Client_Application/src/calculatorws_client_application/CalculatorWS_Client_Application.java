2cc484af3791f814dcd9d2c36514415b1216e678
estudiante <estudiante@estudiante>
2017-10-14 16:52:20 -0500

    Use server add on main application

--- CalculatorWS_Client_Application/src/calculatorws_client_application/CalculatorWS_Client_Application.java
+++ CalculatorWS_Client_Application/src/calculatorws_client_application/CalculatorWS_Client_Application.java
@@ -15,7 +15,22 @@ public class CalculatorWS_Client_Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try{
+            int i = 8;
+            int j = 8;
+            int result = add(i, j);
+            System.out.println("Result = " + result);
+        }catch (Exception ex){
+            System.out.println("Exception: " + ex);
+        }
     }
+
+    private static int add(int i, int j) {
+        org.me.calculator.CalculatorWS_Service service = new org.me.calculator.CalculatorWS_Service();
+        org.me.calculator.CalculatorWS port = service.getCalculatorWSPort();
+        return port.add(i, j);
+    }
+    
+    
     
 }
